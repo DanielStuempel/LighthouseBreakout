c72205f7ae12754ed1c441a317518cde0476daa7
janbo <janbo@Jan-Laptop>
2018-02-27 13:32:53 +0100

    last

--- LighthouseBreakout/src/stuff/Ball.java
+++ LighthouseBreakout/src/stuff/Ball.java
@@ -10,11 +10,17 @@ public class Ball {
 		int posInt = pos[0]*28+pos[1] ;
 		return posInt;
 	}
+	public static double[] getSpeed() {
+		return speed;
+	}
+	public static void setSpeed(double[] newSpeed) {
+		speed = newSpeed;
+	}
 	public static void move() {
-		speed[0] = (pos[0]+speed[0]) > 13 ? speed[0] = -speed[0] : speed[0];
-		speed[0] = (pos[0]+speed[0]) < 0  ? speed[0] = -speed[0] : speed[0];
-		speed[1] = (pos[1]+speed[1]) > 27 ? speed[1] = -speed[1] : speed[1];
-		speed[1] = (pos[1]+speed[1]) < 0 ? speed[1] = -speed[1] : speed[1];
+		speed[0] = pos[0]+speed[0] > 13 ? speed[0] = -speed[0] : speed[0];
+		speed[0] = pos[0]+speed[0] < 0  ? speed[0] = -speed[0] : speed[0];
+		speed[1] = pos[1]+speed[1] > 27 ? speed[1] = -speed[1] : speed[1];
+		speed[1] = pos[1]+speed[1] < 0 ? speed[1] = -speed[1] : speed[1];
 		
 		pos[0] += (int)speed[0];
 		pos[1] += (int)speed[1];
