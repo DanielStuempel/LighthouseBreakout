1877de7d049e56a7ef79858c5f1f944040f9392e
Daniel <Daniel@Notebook>
2018-02-28 22:11:53 +0100

    works but lags sometimes in eclipse

--- LighthouseBreakout/src/stuff/TickTimer.java
+++ LighthouseBreakout/src/stuff/TickTimer.java
@@ -1,9 +1,27 @@
 package stuff;
 
-import java.util.Timer;
+import java.util.TimerTask;
 
-public class TickTimer extends Timer {
-	public TickTimer() {
-		super();
+public abstract class TickTimer extends TimerTask {
+	private double fps;
+	private long lastTick;
+	private long tickCount;
+	
+	@Override
+	public void run() {
+		fps = 1_000_000_000.0 / (System.nanoTime() - lastTick);
+		tick();
+		lastTick = System.nanoTime();
+		tickCount++;
+	}
+	
+	public abstract void tick();
+	
+	public double getCurrentFPS() {
+		return fps;
+	}
+	
+	public long getTickCount() {
+		return tickCount;
 	}
 }
