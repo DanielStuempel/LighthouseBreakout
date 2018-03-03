a738dd4706d46b125eca89f8b132267756c3d9e1
Daniel <Daniel@Notebook>
2018-03-03 18:35:31 +0100

    added settings

---
+++ LighthouseBreakout/src/stuff/Settings.java
@@ -0,0 +1,16 @@
+package stuff;
+
+public class Settings {
+	//display settings
+	public static int FRAME_TICK_MS = 20;
+	public static boolean SHOW_FPS_ON_DISPLAY = true;
+	public static boolean SHOW_RASTER_ON_DISPLAY = true;
+	
+	//game engine settings
+	public static int GAME_TICK__MS = 100;
+	
+	//animation settings
+	public static boolean DO_ANIMATIONS = true;
+	public static boolean PLAYER_INVINCEBLE = false;
+	public static boolean BRICK_COLLISION = true;
+}
